beebe44d0b2fa8f13a58f20a26d92f15db663836
寂月 <3071952083@qq.com>
2026-07-22 23:05:30 +0800

    build.bat 增加部署源码到模组目录

--- TailorMade-ZhCN/TailorMadeUnlockFix/build.bat
+++ TailorMade-ZhCN/TailorMadeUnlockFix/build.bat
@@ -13,9 +13,17 @@ if %ERRORLEVEL% EQU 0 (
     echo   Output: Assemblies\TailorMadeUnlockFix.dll
     echo.
     echo Deploying to RimWorld Mods...
-    xcopy /E /Y /Q Assemblies\TailorMadeUnlockFix.dll "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Assemblies\"
-    xcopy /E /Y /Q About\About.xml "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\About\"
-    xcopy /E /Y /Q About\Preview.png "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\About\"
+    if not exist "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Source" mkdir "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Source"
+    xcopy /E /Y /Q /I Assemblies\TailorMadeUnlockFix.dll "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Assemblies\"
+    xcopy /E /Y /Q /I About\About.xml "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\About\"
+    xcopy /E /Y /Q /I About\Preview.png "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\About\"
+    xcopy /E /Y /Q /I Source\TailorMadeUnlockFix\*.cs "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Source\"
+    xcopy /E /Y /Q /I Source\TailorMadeUnlockFix\*.csproj "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Source\"
+    xcopy /E /Y /Q /I Source\TailorMadeUnlockFix\Directory.Build.props "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Source\"
+    if exist "Source\TailorMadeUnlockFix\Properties" (
+        if not exist "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Source\Properties" mkdir "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Source\Properties"
+        xcopy /Y /Q "Source\TailorMadeUnlockFix\Properties\*.cs" "D:\app\game\steam\steamapps\common\RimWorld\Mods\TailorMadeUnlockFix\Source\Properties\"
+    )
     echo ✓ Deployed!
     echo ✓ Deployed!
 ) else (
