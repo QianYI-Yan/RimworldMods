f40201b0a1d9fec0bc155599afab1356a1336f55
寂月 <3071952083@qq.com>
2026-08-02 17:18:21 +0800

    重新排除原始参考文件（OriginalMod/ 与 OriginalSRC-SDK/），改回公开仓库默认推送

--- TailorMade-ZhCN/OriginalSRC-SDK/TailorMadeUnifiedApparelBodyRefitting/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.Versioning;
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[module: RefSafetyRules(11)]
